e4f48c5ceaa663b8bcc140d834506ef2ae85bb63
Magdalena <miomayska18@codingburgas.bg>
2021-06-26 22:53:03 +0300

    Add: *getLakes() *getAllLakes() *aforementioned functions used to be one function *parameter of type nanodbc to all functions that require it
    Co-authored-by: yistoyanova18 <yistoyanova18@codingburgas.bg>

--- Aqua/Aqua/Aqua.cpp
+++ Aqua/Aqua/Aqua.cpp
@@ -2,14 +2,15 @@
 #include <string>
 #include <nanodbc.h>
 #include <exception>
+#include <vector>
 
 using namespace std;
 
-void runProgram();
-void displayMenu();
-void info();
-void add();
-void search();
+void runProgram(nanodbc::connection conn);
+//void displayMenu();
+//vector<LAKE> getAllLakes(nanodbc::connection conn);
+//void add();
+//void search();
 
 struct LAKE {
 	int id = 0;
@@ -23,72 +24,93 @@ struct LAKE {
 	double maxDepth = 0;
 };
 
-/*void defaultLakes(LAKE* lakes[]) {
-	*lakes[0] = { "Urdino", "Rila", "Urdina", "Cherni Iskur", 2278, 2.53, 89.5, 6.6 };
-	*lakes[1] = { "Alekovo", "Rila", "Bistrica", "Iskur", 2545, 2.39, 135.5, 14.5 };
-	*lakes[2] = { "Marichino", "Rila", "Marica", "Marica", 2378, 2.15, 92.3, 10.8 };
-	*lakes[3] = { "Ribno", "Rila", "Gorna Banenska", "Bqla Mesta", 2191, 2.87, 70, 4.5 };
-	*lakes[4] = { "Murtvo", "Rila", "Gorna Banenska", "Bqla Mesta", 2292, 4.27, 274, 16.5 };
-	*lakes[5] = { "Okoto", "Rila", "Jerman", "Struma", 2440, 6.8, 860, 37.5 };
-	*lakes[6] = { "Bubreka", "Rila", "Jerman", "Struma", 2282, 8.5, 1170, 28 };
-	*lakes[7] = { "Bliznaka", "Rila", "Jerman", "Struma", 1143, 9.1, 590, 27.5 };
-	*lakes[8] = { "Gramatikovo", "Rila", "Jerman", "Struma", 2216, 2.6, 54, 6.5 };
-	*lakes[9] = { "Sedmoto", "Rila", "Jerman", "Struma", 2095, 5.9, 240, 11 };
-	*lakes[10] = { "Gorno Vasilashko", "Pirin", "Demqnica", "Glazne", 2154, 2.32, 28.7, 3.3 };
-	*lakes[11] = { "Ribno Vasilashko", "Pirin", "Demqnica", "Glazne", 2126, 2.59, 41.6, 3.1 };
-	*lakes[12] = { "Popovo", "Pirin", "Retije", "Mesta", 2234, 12.36, 270, 29.5 };
-	*lakes[13] = { "Vlahinsko", "Pirin", "Vlahinska", "Struma", 2302, 6.34, 421.1, 13.4 };
-	*lakes[14] = { "Chairsko", "Pirin", "Chairska", "Bashliica", 2416, 2.26, 47.3, 3.4 };
-}*/
 
-void info() {
-	LAKE lakes[500];
+vector<LAKE> getLakes(nanodbc::connection conn) {
 
-	int choice;
+	vector<LAKE> lakes;
 
-	lakes[0] = { 0, "Urdino", "Rila", "Urdina", "Cherni Iskur", 2278, 2.53, 89.5, 6.6 };
-	lakes[1] = { 1, "Marichino", "Rila", "Marica", "Marica", 2378, 2.15, 92.3, 10.8 };
-	lakes[2] = { 2, "Okoto", "Rila", "Jerman", "Struma", 2440, 6.8, 860, 37.5 };
-	lakes[3] = { 3, "Bubreka", "Rila", "Jerman", "Struma", 2282, 8.5, 1170, 28 };
-	lakes[4] = { 4, "Sedmoto", "Rila", "Jerman", "Struma", 2095, 5.9, 240, 11 };
-	lakes[5] = { 5, "Popovo", "Pirin", "Retije", "Mesta", 2234, 12.36, 270, 29.5 };
+	nanodbc::statement statement(conn); //zashto statemnt can't be prepared???
+	nanodbc::prepare(statement, NANODBC_TEXT(R"( 
+        SELECT *
+            FROM AQUA_Lakes.Lakes
+    )"));
 
-	for (int i = 0; i < 6; i++) {
-		cout << " Lake: " << lakes[i].name << endl;
-		cout << " Mountain: " << lakes[i].mountain << endl;
-		cout << " Beginning river: " << lakes[i].beginRiver << endl;
-		cout << " Valley: " << lakes[i].mainRiver << endl;
-		cout << " Sea level hight: " << lakes[i].seaLevelHeight << endl;
-		cout << " Area: " << lakes[i].area << endl;
-		cout << " Volume: " << lakes[i].volume << endl;
-		cout << " Maximal depth: " << lakes[i].maxDepth << endl << endl;
+	auto result = execute(statement);
+
+	while (result.next())
+	{
+		LAKE lake;
+		lake.id = result.get<int>("Id");
+		lake.name = result.get<nanodbc::string>("LakeName", "");
+		lake.mountain = result.get<nanodbc::string>("Mountain", "");
+		lake.beginRiver = result.get<nanodbc::string>("BeginRiver", "");
+		lake.mainRiver = result.get<nanodbc::string>("Valley", "");
+		lake.seaLevelHeight = result.get<int>("Altitude");
+		lake.area = result.get<float>("Area");
+		lake.volume = result.get<float>("Volume");
+		lake.maxDepth = result.get<float>("MaxDepth");
+
+		lakes.push_back(lake);
 	}
 
+	return lakes;
+}
+
+void getAllLakes(nanodbc::connection conn)
+{
+	vector<LAKE> lakes = getLakes(conn);
+	for (size_t i = 0; i < lakes.size(); i++)
+	{
+		cout << lakes[i].id << " ";
+		cout << lakes[i].name << " ";
+		cout << lakes[i].mountain << " ";
+		cout << lakes[i].beginRiver << " ";
+		cout << lakes[i].mainRiver << " ";
+		cout << lakes[i].seaLevelHeight << " ";
+		cout << lakes[i].area << " ";
+		cout << lakes[i].volume << " ";
+		cout << lakes[i].maxDepth << endl;
+	}
+
+	int choice;
 	cout << endl << "Enter \"1\" to get back to the main menu: ";
 	cin >> choice;
 
-	if (choice == 1) runProgram();
+	if (choice == 1) runProgram(conn);
+	
 }
 
-void add() {
+void insertLake(nanodbc::connection conn) { //kakvo da pravim za da ne vuvejdame id
+
+	nanodbc::statement statement(conn);
+	nanodbc::prepare(statement, NANODBC_TEXT(R"(
+        INSERT INTO
+            AQUA_Lakes.Lakes
+            (first_name, last_name, phone, email, street, city, state, zip_code)
+            VALUES
+            (?, ?, ?, ?, ?, ?, ?, ?)
+    )"));
+
+
+
 	int a;
 	cout << "___add\n";
 	cout << "type '0' to return bach to the menu\n";
 	cin >> a;
 	if (a == 0) {
 		system("cls");
-		runProgram();
+		runProgram(conn);
 	}
 }
 
-void search() {
+void search(nanodbc::connection conn) {
 	int a;
 	cout << "___search\n";
 	cout << "type '0' to return bach to the menu\n";
 	cin >> a;
 	if (a == 0) {
 		system("cls");
-		runProgram();
+		runProgram(conn);
 	}
 }
 
@@ -113,8 +135,7 @@ void displayMenu() {
 	cout << " |_______________________________________________________________________|\n";
 }
 
-void runProgram() {
-	LAKE lakes[500];
+void runProgram(nanodbc::connection conn) {
 	int choice;
 	system("cls");
 	displayMenu();
@@ -123,17 +144,17 @@ void runProgram() {
 		switch (choice) {
 			case 1: {
 				system("cls");
-				info();
+				getAllLakes(conn);
 				break;
 			}
 			case 2: {
 				system("cls");
-				add();
+				insertLake(conn);
 				break;
 			}
 			case 3: {
 				system("cls");
-				search();
+				search(conn);
 				break;
 			}
 			case 4: {
@@ -148,13 +169,14 @@ void runProgram() {
 
 int main() 
 {
-	//runProgram();
+	
 	try
 	{
-		nanodbc::string connstr = NANODBC_TEXT("DRIVER={ODBC Driver 17 for SQL Server};SERVER=(local)\\SQLExpress;DATABASE=AQUA_Lakes;Trusted_Connection=yes;"); // an ODBC connection string to your database
+		nanodbc::string connstr = NANODBC_TEXT("DRIVER={ODBC Driver 17 for SQL Server};SERVER=DESKTOP-VSMRK4C\\SQLExpress;DATABASE=AQUA_Lakes;Trusted_Connection=yes;"); // an ODBC connection string to your database
 
 		nanodbc::connection conn(connstr);
-
+		
+		runProgram(conn);
 
 		return EXIT_SUCCESS;
 	}
